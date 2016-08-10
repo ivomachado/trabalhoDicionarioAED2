81ac02e4310cf6dd42545648b0c959ea014eff95
Ivo Machado <ivo.machado@outlook.com.br>
2016-08-10 11:41:26 -0400

    Inicio do preprocessamento

--- main.c
+++ main.c
@@ -1,4 +1,8 @@
+#include "preprocessamento.h"
+#include "stopwords.h"
+#include "parser.h"
 #include <stdio.h>
+#include <string.h>
 
 void preprocessamento(FILE *fp) {
     
@@ -6,6 +10,10 @@ void preprocessamento(FILE *fp) {
 
 int main(int argc, char **argv) {
     FILE *livro;
+    char palavra[500];
+    int pag = 0, iter = 0;
+    TPreprocessamento *preprocessador;
+    TStopWordManager *stop;
     if(argc < 2) {
         printf("Por favor, digite %s <caminho_relativo_livro>", argv[0]);
     } else {
@@ -13,7 +21,20 @@ int main(int argc, char **argv) {
         if(livro == NULL) {
             printf("Nao foi possivel ler o arquivo %s\n", argv[1]);
         } else {
-
+            stop = criarStopWordManager();
+            preprocessador = criarPreprocessamento();
+            while(lerPalavra(livro, palavra)) {
+                if(strcmp(palavra,"pa") == 0) {
+                    pag++;
+                } else {
+                    iter++;
+                    if(!stop->isStopWord(stop, palavra) || strlen(palavra) > 0) {
+                        preprocessador->incrementarPaginaPalavra(preprocessador, palavra, pag);
+                        printf("%s: %d pag %d iter %d\n", palavra, preprocessador->ocorrenciaPaginaPalavra(preprocessador, palavra, pag), pag, iter);
+                    }
+                }
+            }
         }
     }
+    return 0;
 }
