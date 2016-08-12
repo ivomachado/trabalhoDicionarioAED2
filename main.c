1cda078f9b568cbfdeb9f5776f32158ae793d533
Ivo Machado <ivo.machado@outlook.com.br>
2016-08-12 17:04:04 -0400

    Undo do commit anterior

--- main.c
+++ main.c
@@ -38,7 +38,7 @@ int main(int argc, char **argv) {
                         iter++;
                         preprocessador->incrementarPaginaPalavra(preprocessador, palavra, pag);
                     } else {
-                        preprocessador->incrementarPaginaPalavra(preprocessador, "PA", pag);
+                        preprocessador->incrementarPaginaPalavra(preprocessador, "pa", pag);
                     }
                 }
             }
